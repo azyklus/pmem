3f160b26dc5b958844e957da138d9179672298a8
Yarot Kell <mechild02@gmail.com>
2021-09-16 22:11:29 -0400

    [BUILD] updated build script.

---
+++ psys.sh
@@ -0,0 +1,59 @@
+#!/bin/zsh
+
+ARGS=$(getopt -a --options b:c:h:t: --long "build:,cmd:,help:,test:" -- "$@")
+
+USAGE="\n\rPSYS BUILD SCRIPT\n\r
+\n\r
+("$0" [-h] [-b <target>] [-t <target>])\n\r
+\n\r
+\r./psys.sh -b or --build | target  | > Builds the program/library\n
+\r./psys.sh -c or --cmd   | command | > Runs the specified comnmand\n
+\r./psys.sh -t or --test  | target  | > Runs tests on the program/library\n
+\r./psys.sh -h or --help              > Displays this help message\n
+\n\r
+"
+
+eval set -- "$ARGS"
+
+while true; do
+  case "$1" in
+    -b|--build)
+      tg="$2"
+      cargo build --target="$tg\.json"
+      exit
+      ;;
+    -h|--help)
+      echo $USAGE
+      exit
+      ;;
+    -c|--cmd)
+      case "$2" in
+        "help")
+          echo $USAGE
+          exit
+          ;;
+        "build")
+          target="$4"
+          cargo build --target="./$target.json"
+          exit
+          ;;
+        "test")
+          target="$4"
+          cargo test --target="./$target.json"
+          exit
+          ;;
+        --)
+          break
+          ;;
+      esac
+      exit
+      ;;
+    -t|--test)
+      target="$2"
+      cargo test --target="./$target\.json"
+      exit
+      ;;
+    --)
+      break;;
+  esac
+done
