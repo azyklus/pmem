d3ab35e3b45b463485e7cdcce61a47a5f882cb53
Yarot Kell <mechild02@gmail.com>
2021-09-18 16:59:07 -0400

    [CONFIG] updated dependencies.

--- lib.rs
+++ lib.rs
@@ -4,25 +4,29 @@
 This library provides facilities for managing memory within P-systems.
 */
 
-#![cfg_attr(not(test), no_std)]
+#![no_std]
 #![deny(clippy::all)]
 #![warn(missing_docs)]
 #![allow(unused)]
 #![allow(dead_code)]
-#![feature(alloc_error_handler)]
 #![feature(asm)]
+#![feature(custom_test_frameworks)]
 #![feature(decl_macro)]
 #![feature(lang_items)]
 #![feature(layout_for_ptr)]
 #![feature(llvm_asm)]
 #![feature(slice_ptr_get)]
 #![feature(slice_ptr_len)]
+#![test_runner(ptest::runner)]
 
 extern crate alloc;
 extern crate core;
 
 #[macro_use]
 extern crate lazy_static;
+
+extern crate pcore;
+extern crate ptest;
 
 #[cfg(feature="sync")]
 pub extern crate spin;
@@ -40,7 +44,7 @@ pub mod volatile;
 #[cfg(test)]
 mod tests
 {
-   #[test]
+   #[test_case]
    fn it_works()
    {
       assert_eq!(2 + 2, 4);
