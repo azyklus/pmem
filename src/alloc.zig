420f53a48fc9a3a62a7d4e2937e197f7407a26c6
Yarot Kell <mechild02@gmail.com>
2021-11-02 03:08:15 -0400

    [STYLE] last second style edit for layout module.

--- src/alloc.zig
+++ src/alloc.zig
@@ -1,15 +1,14 @@
-pub const Allocator = global.Global;
-pub const AllocError = errors.AllocError;
-pub const AllocOptions = options.AllocOptions;
-
 /// Here, we have our errors.
 pub const errors = @import("alloc/errors.zig");
-
-/// The `Layout` type defines the memory layout used in the allocation of memory via our allocator.
-pub const layout = @import("alloc/layout.zig");
 
 /// the `Global` type is our global allocator.
 pub const global = @import("alloc/global.zig");
 
 /// Options for allocating memory.
 pub const options = @import("alloc/options.zig");
+
+
+pub const Global = @import("alloc/Global.zig");
+pub const Layout = @import("alloc/Layout.zig");
+pub const AllocError = errors.AllocError;
+pub const AllocOptions = options.AllocOptions;
